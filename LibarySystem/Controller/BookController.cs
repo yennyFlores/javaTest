c28c8d22dbb6985590e270a27122a28ef7a37a54
yennyFlores <yennyd.data@gmail.com>
2024-05-23 01:18:06 -0400

    search working

--- LibarySystem/Controller/BookController.cs
+++ LibarySystem/Controller/BookController.cs
@@ -6,24 +6,26 @@ namespace LibarySystem.Controller;
 
 public class BookController
 {
-     public static void CreateBook(string name, string author, string genre ){
+    /* public static void CreateBook(string name, string author, string genre ){
           Book newBook = new Book(name, author, genre );  
           Console.WriteLine($"Your Recommended Book {newBook.title} was submitted sucessfully!");
           BookStorage.StoreBook(newBook);
 
      }
+     */
 
   public static List<Book> BookSearch(string userSearch ){
           Console.WriteLine($"Searching ...");
                string cmdText =
-                    @"SELECT TOP 10 barcode, title, author, genre FROM books";
+                    @"SELECT rank() over(order by title desc) as _index, barcode, title, author, genre FROM books " + userSearch;
           List<Book> returnBookSearch = new List<Book>();
           using SqlConnection connection = new SqlConnection(ADOconnect.ConnectionString());
           connection.Open();
           using SqlCommand cmd = new SqlCommand(cmdText, connection);
           using SqlDataReader reader = cmd.ExecuteReader();
           while(reader.Read()){
-               Book book = new Book( reader.GetString(1), reader.GetString(2), reader.GetString(3));
+               //Console.WriteLine("pass");
+               Book book = new Book( Convert.ToInt32(reader[0].ToString()), Convert.ToInt32(reader[1].ToString()), reader.GetString(2), reader.GetString(3), reader.GetString(4));
                returnBookSearch.Add(book);
           }
           return returnBookSearch;
--- LibarySystem/UI/Menu.cs
+++ LibarySystem/UI/Menu.cs
@@ -39,13 +39,13 @@ class Menu
                ScreenOne();
                 break;
             case 1: 
-                //MenuBookCreation();
+                BookSearchSelection();
                 break;
             case 2: //View Currently Checkout Books
                 //CheckedOutBooks();
                 break;
             case 3: // Search by Book or Author [add to cart option for each login session] 
-                BookSearchSelection();
+  
                 break;
             case 4: //view checkout cart
                 //CurrentCheckedOutCart();
@@ -67,7 +67,7 @@ class Menu
              1 - Login
              2 - Create Account
              " );
-           string stringSelection = Console.ReadLine();
+           string stringSelection = Console.ReadLine() ?? "";
            int intSelection;
            if( int.TryParse(stringSelection, out intSelection) == false){
                 //throw new MenuSelectionError(string.Format("Your entry {0} is not a valid number. Please enter valid number", stringSelection));
@@ -80,57 +80,25 @@ class Menu
             return intSelection;
     }
 
-    /*
-    public static int MenuOneSelection() {
-             /*if(int.TryParse(message, out int goodSelection) == true){
-                Console.WriteLine("here Again " +goodSelection);
-                return goodSelection;
-                
-            }
-             //return  MenuOneSelection(string.Format("{0}", intSelection));
-            
-            Console.WriteLine(@"
-            {0}
-            Menu Option  
-             0 - Exit
-             1 - Login
-             2 - Create Account
-             ", message );
-           string stringSelection = Console.ReadLine();
-           int intSelection;
-           if( int.TryParse(stringSelection, out intSelection) == false){
-                //throw new MenuSelectionError(string.Format("Your entry {0} is not a valid number. Please enter valid number", stringSelection));
-                Console.WriteLine("error entry");
-               MenuOneSelection();
-            } else if (intSelection < 0 || intSelection > 2) {
-                      Console.WriteLine("error options");
-               MenuOneSelection();
-            }
-            return intSelection;
-
-    }
-    */
-
      public static int MenuTwoSelection() {
            
             Console.WriteLine(@"
             Welcome Back {0} to the library!
             Menu Option   
             0 - Logout
-            1 - Submit a Recommended Book
-            2 - View Currently Checked out Books
-            3 - Search by Name, Author, Barcode, Genre
-            4 - View Selected Cart for Checkout
-            5 - Checkin Books
+            1 - Search by Title
+            2 - View Selected Cart for Checkout 
+            3 - View Currently Checked out Books
+            4 - Checkin Books
              " , UserController.GetCurrentUserName());
-            string stringSelection = Console.ReadLine();
+            string stringSelection = Console.ReadLine() ?? "";
             int intSelection;
 
             if( int.TryParse(stringSelection, out intSelection) == false){
                Console.WriteLine("Your entry {0} is not a valid number. Please enter valid number", stringSelection);
                MenuTwoSelection();
-            } else if (intSelection < 0 || intSelection > 6) {
-               Console.WriteLine("Your entry {0} is not a valid option number. Please enter valid options 0 to 5", stringSelection);
+            } else if (intSelection < 0 || intSelection > 4) {
+               Console.WriteLine("Your entry {0} is not a valid option number. Please enter valid options 0 to 4", stringSelection);
                MenuTwoSelection();
             } 
 
@@ -178,6 +146,7 @@ class Menu
             return userInput;
      }
 
+/*
      public static void MenuBookCreation() {
 
             Console.WriteLine("Please enter your recommened book name:");
@@ -201,18 +170,73 @@ class Menu
                 MenuUserCreation(string.Format("Username {0} already exists. Please try another username:", userInput));
             } else { 
                 UserController.CreateUser(userInput);
-            }*/
+            }
 
      }
-
+*/
      public static void BookSearchSelection(){
-        Console.WriteLine("Please enter your book search by Title or Author:");
-        string userSearch = Console.ReadLine() ?? "";
-        userSearch = userSearch.Trim();
+        Console.WriteLine("Please enter your book search by Title:");
+        string[] inputs = Console.ReadLine().Split(' ');
+
+        List<string> inputsCleaned = new List<String>();
+        for(int i = 0; i < inputs.Length; i++){
+            if(inputs[i].ToLower() != "the" && inputs[i].ToLower() != "of" && inputs[i].ToLower() != "be" && inputs[i].ToLower() != "to" && inputs[i].ToLower() != "and" && inputs[i].ToLower() != "a" && inputs[i].ToLower() != "an" && inputs[i].ToLower() != "i"  ){
+          
+                Console.WriteLine(inputs[i].ToLower());
+                inputsCleaned.Add(inputs[i].ToLower());
+            }
+        }
+
+        string whereClause = "";
+        for(int i = 0; i < inputsCleaned.Count; i++){
+                if (i == 0) {
+                whereClause += "where title like '%" + inputsCleaned[i] + "%' ";
+                } else if(i == inputsCleaned.Count-1){
+                whereClause += "or title like '%" + inputsCleaned[i] + "%' ";
+                } else {
+                whereClause += "or title like '%" + inputsCleaned[i] + "%' ";
+                }
+        }
+        Console.WriteLine(whereClause);
         
-        List<Book> bookResultList= BookController.BookSearch(userSearch);
-        foreach(Book book in bookResultList ){
-            Console.WriteLine(book.title);
+        List<Book> bookResultList= BookController.BookSearch(whereClause);
+     
+        if(bookResultList.Count > 0){
+             Console.WriteLine(@"Results: Please select a book to add to cart ");
+            foreach(Book book in bookResultList ){
+               
+                Console.WriteLine(@"" + book.index + " Title: " + book.title + " Author: " + book.author );
+            }
+            
+            string stringSelection = Console.ReadLine() ?? "";
+            int intSelection = Convert.ToInt16(stringSelection);
+            
+            /*
+            if( int.TryParse(stringSelection, out intSelection) == false){
+               Console.WriteLine("Your entry {0} is not a valid number. Please enter valid number", stringSelection);
+            } else if (intSelection < 0 || intSelection > 4) {
+               Console.WriteLine("Your entry {0} is not a valid option number. Please enter valid options 0 to 4", stringSelection);
+            } 
+            */
+
+           
+            foreach(Book book in bookResultList ){
+                if(intSelection == book.index ) {
+                    //Add to cart method, as a list? 
+                     Console.WriteLine(@"Added to CART" + book.index + " Title: " + book.title + " Author: " + book.author + "" );
+                }
+               
+            }
+            
+
+            
+
+
+            
+
+        } else {
+            Console.WriteLine(@"Results: Sorry. No books match critera. Returning to Main Menu ");
+            MenuTwoSelection();
         }
      }
 
